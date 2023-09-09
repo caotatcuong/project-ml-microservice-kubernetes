15500e801bd585ce2e7ae4ebe6dcadb2a0d55fa1
Your Name <you@example.com>
2023-09-09 22:19:47 +0700

    submit project

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="cuoncgct1/bostonhousingprediction"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run bostonhousingprediction\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=bostonhousingprediction
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward bostonhousingprediction 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="cuoncgct1/bostonhousingprediction"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login &&\
+    docker image tag bostonhousingprediction $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
